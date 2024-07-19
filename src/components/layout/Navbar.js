44845717d2b3f7f15b535c2e22895611f1af16e2
konstantinmedvedev <medvedevblockchain@yandex.ru>
2024-07-19 20:47:47 +0500

    Стилизация Navibar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,29 +1,29 @@
+// src/components/layout/Navbar.js
+
 import * as React from 'react';
 import { FaBell } from 'react-icons/fa';
-import ConnectButton from '../web3/ConnectButton.tsx';
-import Notifications from "../../pages/Notifications";
-import './Navbar.css'; // Подключаем CSS файл
+import ConnectButton from '../web3/ConnectButton';
+import './Navbar.css';
 
 const Navbar = () => {
-    const avatarUrl = "https://backend.ddapps.io/storage/uploads/9/t/8/6/f/vAgMqFWkaqSd9Rtfzi7q6dMZsixTng7AUQBfcdZI.jpg"; // Ссылка на изображение-заглушку
+    const avatarUrl = "https://backend.ddapps.io/storage/uploads/9/t/8/6/f/vAgMqFWkaqSd9Rtfzi7q6dMZsixTng7AUQBfcdZI.jpg";
 
     return (
-        <nav className="bg-[#F5F7F8] p-4 flex justify-between items-center">
-            <div className="flex items-center space-x-2">
-                <span className="text-gray-500">Dashboard /</span>
-                <span className="text-black font-bold">Home</span>
-            </div>
-            <div className="flex items-center">
-                <input
-                    type="text"
-                    placeholder="Search"
-                    className="bg-gray-100 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-200"
-                />
-                <div className="avatar-container mx-2">
-                    <img src={avatarUrl} alt="User Avatar" className="avatar-icon" />
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="logo">Go DAO Software</div>
+                <div className="actions">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="search-input"
+                    />
+                    <div className="avatar-container">
+                        <img src={avatarUrl} alt="User Avatar" className="avatar-icon" />
+                    </div>
+                    <ConnectButton />
+                    <button className="sign-in-button">Личный кабинет</button>
                 </div>
-                <ConnectButton /> {ConnectButton}
-                <button className="sign-in-button mx-2">Личный кабинет</button> {ConnectButton}
             </div>
         </nav>
     );
