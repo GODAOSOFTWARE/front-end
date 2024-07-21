c38a45cf9313d7586e9a6c10ad97304dda7084a8
konstantinmedvedev <medvedevblockchain@yandex.ru>
2024-07-22 01:04:17 +0500

    Стилизация интерфейса

--- src/components/web3/ConnectButton.tsx
+++ src/components/web3/ConnectButton.tsx
@@ -1,20 +1,7 @@
 // src/components/web3/ConnectButton.tsx
 
 import React from 'react';
-import { useWeb3Modal } from '@web3modal/wagmi/react';
-import './ConnectButton.css';  // Импортируем стили
 
-const ConnectButton: React.FC = () => {
-    const { open } = useWeb3Modal();
-
-    return (
-        <button
-            onClick={() => open()}
-            className="connect-button"
-        >
-            Подключить кошелек
-        </button>
-    );
-};
-
-export default ConnectButton;
+export default function ConnectButton() {
+    return <w3m-button />
+}
