40d99dcf0c94ddf023684698501796d1bf34d1ab
konstantinmedvedev <medvedevblockchain@yandex.ru>
2024-08-05 11:32:44 +0500

    Добавлена таблица пользователей

--- src/components/tables/MembersTable.js
+++ src/components/tables/MembersTable.js
@@ -1,7 +1,6 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { PencilIcon, UserPlusIcon } from "@heroicons/react/24/solid";
 import { Card, CardHeader, Input, Typography, Button, CardBody, Chip, CardFooter, Tabs, TabsHeader, Tab, Avatar, IconButton, Tooltip } from "@material-tailwind/react";
-import styles from '../../styles/MembersTable.module.css';  // Обновите путь к файлу стилей
 
 const TABS = [
     {
@@ -70,9 +69,9 @@ const TABLE_ROWS = [
 
 export function MembersTable() {
     return (
-        <Card className={styles.tableContainer}>
-            <CardHeader floated={false} shadow={false} className={styles.cardHeader}>
-                <div className={styles.cardHeaderInner}>
+        <Card className="h-full w-full">
+            <CardHeader floated={false} shadow={false} className="rounded-none">
+                <div className="mb-8 flex items-center justify-between gap-8">
                     <div>
                         <Typography variant="h5" color="blue-gray">
                             Members list
@@ -90,7 +89,7 @@ export function MembersTable() {
                         </Button>
                     </div>
                 </div>
-                <div className={styles.cardHeaderInnerMd}>
+                <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
                     <Tabs value="all" className="w-full md:w-max">
                         <TabsHeader>
                             {TABS.map(({ label, value }) => (
@@ -100,7 +99,7 @@ export function MembersTable() {
                             ))}
                         </TabsHeader>
                     </Tabs>
-                    <div className={styles.inputSearch}>
+                    <div className="w-full md:w-72">
                         <Input
                             label="Search"
                             icon={<MagnifyingGlassIcon className="h-5 w-5" />}
@@ -109,13 +108,13 @@ export function MembersTable() {
                 </div>
             </CardHeader>
             <CardBody className="overflow-scroll px-0">
-                <table className={styles.table}>
+                <table className="mt-4 w-full min-w-max table-auto text-left">
                     <thead>
                     <tr>
                         {TABLE_HEAD.map((head) => (
                             <th
                                 key={head}
-                                className={styles.tableHeader}
+                                className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
                             >
                                 <Typography
                                     variant="small"
@@ -133,8 +132,8 @@ export function MembersTable() {
                         ({ img, name, email, job, org, online, date }, index) => {
                             const isLast = index === TABLE_ROWS.length - 1;
                             const classes = isLast
-                                ? styles.lastTableRow
-                                : styles.tableRow;
+                                ? "p-4"
+                                : "p-4 border-b border-blue-gray-50";
 
                             return (
                                 <tr key={name}>
@@ -205,16 +204,16 @@ export function MembersTable() {
                                     </td>
                                 </tr>
                             );
-                        }
+                        },
                     )}
                     </tbody>
                 </table>
             </CardBody>
-            <CardFooter className={styles.cardFooter}>
+            <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
                 <Typography variant="small" color="blue-gray" className="font-normal">
                     Page 1 of 10
                 </Typography>
-                <div className={styles.cardFooterButtonGroup}>
+                <div className="flex gap-2">
                     <Button variant="outlined" size="sm">
                         Previous
                     </Button>
